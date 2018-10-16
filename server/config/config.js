04bb4e3d467a8593df99a2082a5222d3a39f8f31
Cesar Matheus <matheuss.cx@gmail.com>
2018-10-16 10:28:22 -0300

    Configuracion de producción

--- server/config/config.js
+++ server/config/config.js
@@ -8,11 +8,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //============== DB ===============
 // wscAV83jBjmWxim
-// let urlDB;
-// if(process.env.NODE_ENV === 'dev'){
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// }else{
-     urlDB ='mongodb://rest-server-cafe:wscAV83jBjmWxim@ds047207.mlab.com:47207/rest-server-cafe';
-// }
-
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://rest-server-cafe:wscAV83jBjmWxim@ds047207.mlab.com:47207/rest-server-cafe';
+}
 process.env.URLDB = urlDB;
